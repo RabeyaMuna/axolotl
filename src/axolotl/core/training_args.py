886cb0e53c298715e83c8f4c4dbf6f1bc2474ffa
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-04 22:15:48 -0400

    RabeyaMuna__diff__id_475

--- src/axolotl/core/training_args.py
+++ src/axolotl/core/training_args.py
@@ -3,7 +3,6 @@ extra axolotl specific training args
 """
 
 from dataclasses import dataclass, field
-from typing import Optional
 
 from PIL.Image import Resampling
 from transformers import TrainingArguments
@@ -17,7 +16,7 @@ class AxolotlTrainingMixins:
     """
 
     # pylint: disable=duplicate-code
-    model_type: Optional[str] = field(
+    model_type: str | None = field(
         default=None, metadata={"help": "HF model configuration model_type."}
     )
     lr_quadratic_warmup: bool = field(
@@ -44,7 +43,7 @@ class AxolotlTrainingMixins:
         default=False,
         metadata={"help": "Use real batches for efficient training."},
     )
-    eval_sample_packing: Optional[bool] = field(
+    eval_sample_packing: bool | None = field(
         default=None,
         metadata={"help": "Use sample packing for efficient evals."},
     )
@@ -68,38 +67,38 @@ class AxolotlTrainingMixins:
         default=2048,
         metadata={"help": "The maximum sequence length the model can handle"},
     )
-    relora_steps: Optional[int] = field(
+    relora_steps: int | None = field(
         default=None,
         metadata={"help": "how often to reset for ReLoRA"},
     )
-    relora_warmup_steps: Optional[int] = field(
+    relora_warmup_steps: int | None = field(
         default=None,
         metadata={"help": "how many warmup steps to take after reset for ReLoRA"},
     )
-    relora_anneal_steps: Optional[int] = field(
+    relora_anneal_steps: int | None = field(
         default=None,
         metadata={"help": "how many warmup steps to take after reset for ReLoRA"},
     )
-    relora_prune_ratio: Optional[float] = field(
+    relora_prune_ratio: float | None = field(
         default=0.9,
         metadata={"help": "prune ratio for magnitude pruning of the optimizer"},
     )
-    bench_split: Optional[str] = field(
+    bench_split: str | None = field(
         default="eval", metadata={"help": "The benchmark split to run on"}
     )
-    bench_dataset: Optional[str] = field(
+    bench_dataset: str | None = field(
         default="pharaouk/dharma-1/dharma_1_mini.json",
         metadata={
             "help": "Benchmark dataset to use: options are `mmlu-zs`, `mmlu-fs`, or the full path to the dataset file"
         },
     )
-    do_bench_eval: Optional[bool] = field(
+    do_bench_eval: bool | None = field(
         default=False, metadata={"help": "Whether to run the Benchmark evaluation."}
     )
-    do_causal_lm_eval: Optional[bool] = field(
+    do_causal_lm_eval: bool | None = field(
         default=False, metadata={"help": "Whether to run the Causal LM evaluation."}
     )
-    max_bench_samples: Optional[int] = field(
+    max_bench_samples: int | None = field(
         default=None,
         metadata={
             "help": "If set, only evaluates on `max_bench_samples` of the benchmark dataset."
@@ -108,36 +107,41 @@ class AxolotlTrainingMixins:
     bench_source_max_len: int = field(
         default=2048, metadata={"help": "Maximum source sequence length for bench."}
     )
-    dataloader_prefetch_factor: Optional[int] = field(
+    dataloader_prefetch_factor: int | None = field(
         default=None,
         metadata={"help": "prefetch_factor argument to the dataloader"},
     )
-    cosine_min_lr_ratio: Optional[float] = field(
+    dataset_num_proc: int | None = field(
+        default=None,
+        metadata={"help": "Number of processes to use for dataset loading"},
+    )
+
+    cosine_min_lr_ratio: float | None = field(
         default=None,
         metadata={"help": "Minimum learning rate is min_lr_ratio * learning_rate"},
     )
-    cosine_constant_lr_ratio: Optional[float] = field(
+    cosine_constant_lr_ratio: float | None = field(
         default=None,
         metadata={
             "help": "Starting constant learning rate step is cosine_constant_lr_ratio * max_steps"
         },
     )
-    loraplus_lr_ratio: Optional[float] = field(
+    loraplus_lr_ratio: float | None = field(
         default=None, metadata={"help": "loraplus learning rate ratio lr_B / lr_A."}
     )
-    loraplus_lr_embedding: Optional[float] = field(
+    loraplus_lr_embedding: float | None = field(
         default=1e-6,
         metadata={"help": "loraplus learning rate for lora embedding layers."},
     )
-    embedding_lr_scale: Optional[float] = field(
+    embedding_lr_scale: float | None = field(
         default=None,
         metadata={"help": "Scale the learning rate for the embedding layers."},
     )
-    lr_groups: Optional[list[dict]] = field(
+    lr_groups: list[dict] | None = field(
         default=None,
         metadata={"help": "Specify learning rate groups for with different LRs."},
     )
-    embedding_lr: Optional[float] = field(
+    embedding_lr: float | None = field(
         default=None,
         metadata={"help": "absolute learning rate for the embedding layers."},
     )
@@ -145,76 +149,76 @@ class AxolotlTrainingMixins:
         default=False,
         metadata={"help": "whether this is a qlora training"},
     )
-    orpo_alpha: Optional[float] = field(
+    orpo_alpha: float | None = field(
         default=None,
     )
-    lisa_n_layers: Optional[int] = field(
+    lisa_n_layers: int | None = field(
         default=None,
         metadata={"help": "the number of activate layers in LISA"},
     )
-    lisa_step_interval: Optional[int] = field(
+    lisa_step_interval: int | None = field(
         default=None,
         metadata={"help": "how often to switch layers in LISA"},
     )
-    lisa_layers_attribute: Optional[str] = field(
+    lisa_layers_attribute: str | None = field(
         default=None,
         metadata={"help": "path under the model to access the layers"},
     )
-    curriculum_sampling: Optional[bool] = field(
+    curriculum_sampling: bool | None = field(
         default=None,
         metadata={"help": "whether to use sequential sampling for curriculum learning"},
     )
-    alternate_lr_scheduler_type: Optional[str] = field(
+    alternate_lr_scheduler_type: str | None = field(
         default=None,
         metadata={
             "help": "workaround to pass an alternate lr scheduler to the HF trainer"
         },
     )
-    chat_template: Optional[str] = field(
+    chat_template: str | None = field(
         default=None,
         metadata={"help": "Chat template converting chat messages to text"},
     )
 
-    kd_ce_alpha: Optional[float] = field(
+    kd_ce_alpha: float | None = field(
         default=None,
         metadata={
             "help": "The alpha scaling parameter for SFT cross entropy loss when using KD"
         },
     )
 
-    kd_alpha: Optional[float] = field(
+    kd_alpha: float | None = field(
         default=1.0,
         metadata={"help": "The alpha scaling parameter for KD loss"},
     )
 
-    kd_temperature: Optional[float] = field(
+    kd_temperature: float | None = field(
         default=1.0,
         metadata={
             "help": "the temperature parameter for KL divergence loss when using KD"
         },
     )
 
-    kd_zscore_base_temp: Optional[float] = field(
+    kd_zscore_base_temp: float | None = field(
         default=None,
         metadata={
             "help": "the base temperature parameter for KL divergence with z-score when using KD"
         },
     )
 
-    kd_top_k_before_softmax: Optional[bool] = field(
+    kd_top_k_before_softmax: bool | None = field(
         default=None,
         metadata={
             "help": "Whether to apply top_k_before_softmax to the logits when using KD"
         },
     )
 
-    adam_beta3: Optional[float] = field(
+    adam_beta3: float | None = field(
         default=None,
         metadata={
             "help": "The beta3 hyperparameter used in some optimizers such as CAME"
         },
     )
-    adam_epsilon2: Optional[float] = field(
+    adam_epsilon2: float | None = field(
         default=None,
         metadata={
             "help": "The epsilon2 hyperparameter used in some optimizers such as CAME"
@@ -266,7 +270,7 @@ class AxolotlCPOConfig(AxolotlTrainingMixins, CPOConfig):
     CPO config for CPO training
     """
 
-    simpo_gamma: Optional[float] = field(
+    simpo_gamma: float | None = field(
         default=None,
         metadata={"help": "simpo gamma parameter"},
     )
